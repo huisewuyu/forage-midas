7992a3657b6459b6c96296399bfaa1a19dccc5d0
huisewuyu <90361674+huisewuyu@users.noreply.github.com>
2026-03-26 21:16:38 +0800

    Add files via upload

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,71 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.repository.UserRepository;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.foundation.Incentive;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
-
-    public DatabaseConduit(UserRepository userRepository) {
+    private final TransactionRecordRepository transactionRecordRepository;
+    private final RestTemplate restTemplate = new RestTemplate(); 
+	
+    public DatabaseConduit(UserRepository userRepository,
+                           TransactionRecordRepository transactionRecordRepository) {
         this.userRepository = userRepository;
+        this.transactionRecordRepository = transactionRecordRepository;
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
     }
+	
 
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
+    public void listen(Transaction transaction) {
+		//System.err.println("PARSED TRANSACTION = " + transaction);
+		 
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+		
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            return;
+        }
+		
+		Incentive incentive = restTemplate.postForObject(
+				"http://localhost:8080/incentive",
+				transaction,
+				Incentive.class
+		);
+		
+		float incentiveAmount = 0;
+		if (incentive != null) {
+		    incentiveAmount = incentive.getAmount(); 
+		}
+
+        TransactionRecord transactionRecord = new TransactionRecord(
+            sender, 
+            recipient, 
+            transaction.getAmount(),
+            incentiveAmount
+        ); 
+		transactionRecordRepository.save(transactionRecord);		
+		
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
+		
+        userRepository.save(sender);
+        userRepository.save(recipient);
+		
+    }
 }
